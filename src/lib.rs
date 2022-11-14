a77c85f71899246dc76067bcbf30ad6999fc5bfc
James <11054922+entrancedjames@users.noreply.github.com>
2022-11-13 17:15:52 -0800

    begin creating app tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod constants;
 pub mod contract;
-mod error;
+pub mod error;
 pub mod helpers;
 pub mod models;
 pub mod msg;
--- src/tests/integration.rs
+++ src/tests/integration.rs
@@ -1,9 +1,20 @@
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::{Addr, Empty};
+    use crate::msg::ExecuteMsg;
+    use cosmwasm_std::{Addr, BlockInfo, Empty};
     use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+    use cw_utils::Duration;
 
-    use crate::tests::common::TESTING_INST_MSG;
+    fn expire(voting_period: Duration) -> impl Fn(&mut BlockInfo) {
+        move |block: &mut BlockInfo| {
+            match voting_period {
+                Duration::Time(duration) => block.time = block.time.plus_seconds(duration + 1),
+                Duration::Height(duration) => block.height += duration + 1,
+            };
+        }
+    }
+
+    use crate::tests::common::{TESTING_DURATION, TESTING_INST_MSG};
 
     const USER: &str = "USER";
     const ADMIN: &str = "ADMIN";
@@ -23,19 +34,42 @@ mod tests {
     }
 
     #[test]
-    fn buy_tickets_and_lottery() {
+    fn instantiate_buy_tickets_and_execute() {
         let mut app = mock_app();
         let lotto_code_id = app.store_code(contract_lotto());
 
-        app.instantiate_contract(
-            lotto_code_id,
-            Addr::unchecked(ADMIN),
-            &TESTING_INST_MSG,
-            &[],
-            "yolo",
-            None,
-        )
-        .unwrap();
+        let lotto_contract_addr = app
+            .instantiate_contract(
+                lotto_code_id,
+                Addr::unchecked(ADMIN),
+                &TESTING_INST_MSG,
+                &[],
+                "yolo",
+                None,
+            )
+            .unwrap();
+
+        app.update_block(expire(TESTING_DURATION));
+
+        let buy_ticket_exec_msg = ExecuteMsg::BuyTicket { num_tickets: 1 };
+
+        let app_response_1 = app
+            .execute_contract(
+                Addr::unchecked("TEST_USER_1"),
+                lotto_contract_addr.clone(),
+                &buy_ticket_exec_msg,
+                &[],
+            )
+            .unwrap();
+
+        let app_response_2 = app
+            .execute_contract(
+                Addr::unchecked("TEST_USER_2"),
+                lotto_contract_addr.clone(),
+                &buy_ticket_exec_msg,
+                &[],
+            )
+            .unwrap();
 
         // how do i add a time dilation, such that we can update the stupid as contract.
         // let resp = execute(

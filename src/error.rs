a4a3aaa85ffbb0d8f9f9a5efc6bfb4be074fabc3
James <11054922+entrancedjames@users.noreply.github.com>
2022-11-14 22:40:53 -0800

    add app unit test to make sure state changes do occurr. one that updates to the state are occurring

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,16 @@
 use cosmwasm_std::StdError;
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
     #[error("Unauthorized")]
     Unauthorized {},
+
+    #[error("The ticket buying process right now is closed.")]
+    TicketBuyingNotAvailable {},
 
     #[error("The current lottery is not executable because it is decided or still open.")]
     LotteryNotExecutable {},

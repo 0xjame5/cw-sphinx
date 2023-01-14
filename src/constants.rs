6705a8c8048cfc73f0db988606cde5cbf1ba43a7
James <11054922+entrancedjames@users.noreply.github.com>
2023-01-14 14:46:31 -0800

    Add fee costs for lottery and update instantiate with admin (#5)

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,5 @@
 pub const CONTRACT_NAME: &str = "crates.io:cw-lootboxes";
 pub const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const TOTAL_POOL_SIZE: u64 = 1000000;
+
+pub const MAX_HOUSE_FEE: u64 = 5000; // this is 50%, e.g. 10000 is 100%

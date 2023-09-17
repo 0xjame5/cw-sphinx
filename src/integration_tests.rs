0a37c060f2bc5f6eef30da2eb8945af69ab10550
James <11054922+0xjame5@users.noreply.github.com>
2023-09-17 14:43:45 -0700

    add total number of lottery tickets (#10)

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -39,36 +39,8 @@ mod tests {
     }
 
     #[test]
-    fn instantiate_buy_tickets_and_execute() {
-        let mut app = mock_app(
-            Addr::unchecked(TEST_GOD),
-            vec![Coin {
-                denom: TESTING_NATIVE_DENOM.to_string(),
-                amount: Uint128::new(100_000_000_000u128),
-            }],
-        );
-
-        // God is sending token to two people, admin and user1
-        app.send_tokens(
-            Addr::unchecked(TEST_GOD),
-            Addr::unchecked(TEST_ADMIN),
-            &[Coin {
-                denom: TESTING_NATIVE_DENOM.to_string(),
-                amount: Uint128::new(10_000u128),
-            }],
-        )
-        .unwrap();
-        app.send_tokens(
-            Addr::unchecked(TEST_GOD),
-            Addr::unchecked(TEST_USER_1),
-            &[Coin {
-                denom: TESTING_NATIVE_DENOM.to_string(),
-                amount: Uint128::new(3_000u128),
-            }],
-        )
-        .unwrap();
-
-        let lotto_code_id = app.store_code(contract_lotto());
+    fn instantiate_buy_1_ticket_and_execute() {
+        let (mut app, lotto_code_id) = setup_app();
 
         let instantiate_message = InstantiateMsg {
             ticket_cost: coin(TESTING_TICKET_COST, TESTING_NATIVE_DENOM),
@@ -99,14 +71,14 @@ mod tests {
                 }],
             )
             .unwrap();
-
+        // Validate user only bought 1 ticket and balance is now reflecting now
         assert_eq!(
             app.wrap()
                 .query_balance(Addr::unchecked(TEST_USER_1), TESTING_NATIVE_DENOM)
                 .unwrap(),
             Coin {
                 denom: TESTING_NATIVE_DENOM.to_string(),
-                amount: Uint128::new(2_000u128)
+                amount: Uint128::new(4_000u128)
             }
         );
 
@@ -186,7 +158,8 @@ mod tests {
                 lotto_state: LotteryState::CLOSED {
                     winner: (Addr::unchecked(TEST_USER_1)),
                     claimed: false
-                }
+                },
+                total_tickets: 1
             }
         );
 
@@ -210,7 +183,8 @@ mod tests {
                 lotto_state: LotteryState::CLOSED {
                     winner: (Addr::unchecked(TEST_USER_1)),
                     claimed: true
-                }
+                },
+                total_tickets: 1
             }
         );
 
@@ -234,7 +208,7 @@ mod tests {
                 .unwrap(),
             Coin {
                 denom: TESTING_NATIVE_DENOM.to_string(),
-                amount: Uint128::new(2_950u128)
+                amount: Uint128::new(4_950u128)
             }
         );
 
@@ -248,5 +222,311 @@ mod tests {
                 amount: Uint128::new(10_050u128)
             }
         );
+    }
+
+    #[test]
+    fn instantiate_buy_with_two_players_ticket_and_execute() {
+        let (mut app, lotto_code_id) = setup_app();
+
+        let instantiate_message = InstantiateMsg {
+            ticket_cost: coin(TESTING_TICKET_COST, TESTING_NATIVE_DENOM),
+            lottery_duration: TESTING_DURATION,
+            admin: TEST_ADMIN.to_string(),
+            house_fee: 500, // 5%
+        };
+
+        let lotto_contract_addr = app
+            .instantiate_contract(
+                lotto_code_id,
+                Addr::unchecked(TEST_ADMIN),
+                &instantiate_message,
+                &[],
+                "yolo",
+                None,
+            )
+            .unwrap();
+
+        app.execute_contract(
+            Addr::unchecked(TEST_USER_1),
+            lotto_contract_addr.clone(),
+            &ExecuteMsg::BuyTicket { num_tickets: 1 },
+            &[Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(1_000u128),
+            }],
+        )
+        .unwrap();
+
+        app.execute_contract(
+            Addr::unchecked(TEST_USER_1),
+            lotto_contract_addr.clone(),
+            &ExecuteMsg::BuyTicket { num_tickets: 2 },
+            &[Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(2_000u128),
+            }],
+        )
+        .unwrap();
+
+        app.execute_contract(
+            Addr::unchecked(TEST_USER_2),
+            lotto_contract_addr.clone(),
+            &ExecuteMsg::BuyTicket { num_tickets: 3 },
+            &[Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(3_000u128),
+            }],
+        )
+        .unwrap();
+
+        // Both players balances should've updated.
+        assert_eq!(
+            app.wrap()
+                .query_balance(Addr::unchecked(TEST_USER_1), TESTING_NATIVE_DENOM)
+                .unwrap(),
+            Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(2_000u128)
+            }
+        );
+        assert_eq!(
+            app.wrap()
+                .query_balance(Addr::unchecked(TEST_USER_2), TESTING_NATIVE_DENOM)
+                .unwrap(),
+            Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(2_000u128)
+            }
+        );
+
+        app.update_block(expire(TESTING_DURATION));
+
+        // Note that this would be empty, shit would return OK.
+        // This is because this would update the contract to next state. However,
+        // the next call would fail.
+        app.execute_contract(
+            Addr::unchecked(TEST_USER_2),
+            lotto_contract_addr.clone(),
+            &ExecuteMsg::BuyTicket { num_tickets: 1 },
+            &[],
+        )
+        .unwrap();
+
+        // Validate user 1 and 2 bought 3 tickets only. User 3 bought none.
+        assert_eq!(
+            app.wrap()
+                .query_wasm_smart::<TicketResponse>(
+                    lotto_contract_addr.clone(),
+                    &QueryMsg::TicketCount {
+                        addr: Addr::unchecked(TEST_USER_1),
+                    },
+                )
+                .unwrap(),
+            TicketResponse { tickets: Some(3) }
+        );
+        assert_eq!(
+            app.wrap()
+                .query_wasm_smart::<TicketResponse>(
+                    lotto_contract_addr.clone(),
+                    &QueryMsg::TicketCount {
+                        addr: Addr::unchecked(TEST_USER_2),
+                    },
+                )
+                .unwrap(),
+            TicketResponse { tickets: Some(3) }
+        );
+        assert_eq!(
+            app.wrap()
+                .query_wasm_smart::<TicketResponse>(
+                    lotto_contract_addr.clone(),
+                    &QueryMsg::TicketCount {
+                        addr: Addr::unchecked(TEST_USER_3),
+                    },
+                )
+                .unwrap(),
+            TicketResponse { tickets: None }
+        );
+
+        // Below validate user cannot buy tickets once the state has changed
+        assert_eq!(
+            app.execute_contract(
+                Addr::unchecked(TEST_USER_3),
+                lotto_contract_addr.clone(),
+                &ExecuteMsg::BuyTicket { num_tickets: 1 },
+                &[],
+            )
+            .unwrap_err()
+            .downcast::<ContractError>()
+            .unwrap(),
+            ContractError::TicketBuyingNotAvailable {},
+        );
+
+        // Let admin move lottery forward.
+        app.execute_contract(
+            Addr::unchecked(TEST_ADMIN),
+            lotto_contract_addr.clone(),
+            &ExecuteMsg::ExecuteLottery { seed: 12 },
+            &[],
+        )
+        .unwrap();
+
+        // Lottery state response shows expected state
+        assert_eq!(
+            app.wrap()
+                .query_wasm_smart::<LotteryStateResponse>(
+                    lotto_contract_addr.clone(),
+                    &QueryMsg::LotteryState {}
+                )
+                .unwrap(),
+            LotteryStateResponse {
+                lotto_state: LotteryState::CLOSED {
+                    winner: (Addr::unchecked(TEST_USER_2)),
+                    claimed: false
+                },
+                total_tickets: 6
+            }
+        );
+
+        // Let user 1 try and claim tokens claim tokens and get error
+        assert_eq!(
+            app.execute_contract(
+                Addr::unchecked(TEST_USER_1),
+                lotto_contract_addr.clone(),
+                &ExecuteMsg::ClaimTokens {},
+                &[],
+            )
+            .unwrap_err()
+            .downcast::<ContractError>()
+            .unwrap(),
+            ContractError::LotteryNotClaimedByCorrectUser {}
+        );
+
+        assert_eq!(
+            app.wrap()
+                .query_wasm_smart::<LotteryStateResponse>(
+                    lotto_contract_addr.clone(),
+                    &QueryMsg::LotteryState {}
+                )
+                .unwrap(),
+            LotteryStateResponse {
+                lotto_state: LotteryState::CLOSED {
+                    winner: (Addr::unchecked(TEST_USER_2)),
+                    claimed: false
+                },
+                total_tickets: 6
+            }
+        );
+
+        // Let user 2 claim tokens
+        app.execute_contract(
+            Addr::unchecked(TEST_USER_2),
+            lotto_contract_addr.clone(),
+            &ExecuteMsg::ClaimTokens {},
+            &[],
+        )
+        .unwrap();
+
+        // Validate total tickets, lottery state is closed with claim and winner set.
+        assert_eq!(
+            app.wrap()
+                .query_wasm_smart::<LotteryStateResponse>(
+                    lotto_contract_addr.clone(),
+                    &QueryMsg::LotteryState {}
+                )
+                .unwrap(),
+            LotteryStateResponse {
+                lotto_state: LotteryState::CLOSED {
+                    winner: (Addr::unchecked(TEST_USER_2)),
+                    claimed: true
+                },
+                total_tickets: 6
+            }
+        );
+
+        // Validate contract has no remaining balance and has been flushed out.
+        assert_eq!(
+            app.wrap()
+                .query_balance(lotto_contract_addr, TESTING_NATIVE_DENOM)
+                .unwrap(),
+            Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Default::default(),
+            }
+        );
+
+        // User 2 lost all that was put in.
+        assert_eq!(
+            app.wrap()
+                .query_balance(Addr::unchecked(TEST_USER_1), TESTING_NATIVE_DENOM)
+                .unwrap(),
+            Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(2_000u128)
+            }
+        );
+
+        // User 3 won minus the fees
+        assert_eq!(
+            app.wrap()
+                .query_balance(Addr::unchecked(TEST_USER_2), TESTING_NATIVE_DENOM)
+                .unwrap(),
+            Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(7_700u128)
+            }
+        );
+
+        // Admin should have some more tokens now from fee
+        assert_eq!(
+            app.wrap()
+                .query_balance(Addr::unchecked(TEST_ADMIN), TESTING_NATIVE_DENOM)
+                .unwrap(),
+            Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(10_300u128)
+            }
+        );
+    }
+
+    fn setup_app() -> (App, u64) {
+        // God is genesis, the whole defined sentient
+        let mut app = mock_app(
+            Addr::unchecked(TEST_GOD),
+            vec![Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(100_000_000_000u128),
+            }],
+        );
+        // God is sending token admin, user1 and user 2
+        app.send_tokens(
+            Addr::unchecked(TEST_GOD),
+            Addr::unchecked(TEST_ADMIN),
+            &[Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(10_000u128),
+            }],
+        )
+        .unwrap();
+        app.send_tokens(
+            Addr::unchecked(TEST_GOD),
+            Addr::unchecked(TEST_USER_1),
+            &[Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(5_000u128),
+            }],
+        )
+        .unwrap();
+        app.send_tokens(
+            Addr::unchecked(TEST_GOD),
+            Addr::unchecked(TEST_USER_2),
+            &[Coin {
+                denom: TESTING_NATIVE_DENOM.to_string(),
+                amount: Uint128::new(5_000u128),
+            }],
+        )
+        .unwrap();
+
+        let lotto_code_id = app.store_code(contract_lotto());
+        (app, lotto_code_id)
     }
 }
